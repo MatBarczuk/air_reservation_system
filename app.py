4b2c6c900f8a43bdf15c5cc6ab4dd4e0bc09971f
Mateusz Barczuk <matbarczuk@gmial.com>
2020-10-12 19:25:45 +0200

    full simple reservation system

--- app.py
+++ app.py
@@ -48,6 +48,10 @@ class Flight:
 
     def relocate_passenger(self, seat_from, seat_to):
         row_from, letter_from = self._parse_seat(seat_from)
+
+        if self.seating_plan[row_from][letter_from] is None:
+            raise ValueError(f'Seat is not occupied like {seat_from}')
+
         row_to, letter_to = self._parse_seat(seat_to)
 
         if self.seating_plan[row_to][letter_to] is not None:
@@ -55,6 +59,29 @@ class Flight:
 
         self.seating_plan[row_to][letter_to] = self.seating_plan[row_from][letter_from]
         self.seating_plan[row_from][letter_from] = None
+
+    def num_empty_seats(self):
+        return sum(sum(1 for seat in row.values() if seat is None) for row in self.seating_plan if row is not None)
+
+    def print_cards(self, printer):
+        passengers = self.get_passengers()
+
+        for passenger, seat in passengers:
+            printer(passenger, seat, self.get_airplane_model(), self.flight_number)
+
+    def get_passengers(self):
+        passengers = []
+
+        rows, letters = self.airplane.seating_plan()
+
+        for row in rows:
+            for letter in letters:
+                passenger = self.seating_plan[row][letter]
+                if passenger is not None:
+                    passenger_data = passenger, f'{row}{letter}'
+                    passengers.append(passenger_data)
+
+        return passengers
 
 
 class Airplane:
@@ -83,6 +110,16 @@ class AirbusA370(Airplane):
         return range(1, 40), 'ABCDEGHJK'
 
 
+def card_printer(name, seat, plane_model, flight_number):
+    text = f"| Pasażer: {name}, Siedzienie: {seat}, Model: {plane_model}, Numer lotu: {flight_number} |"
+    frame = f"+{'-' * (len(text) - 2)}+"
+    empty_frame = f"|{' ' * (len(text) - 2)}|"
+
+    border = [frame, empty_frame, text, empty_frame, frame]
+    border_text = "\n".join(border)
+    print(border_text)
+
+
 f = Flight('BA123', Boeing737())
 print(f.get_airlines())
 print(f.get_number())
@@ -101,4 +138,8 @@ f.allocate_passenger('Paweł K', '1C')
 
 f.relocate_passenger('24A', '22G')
 
-pprint(f.seating_plan)
+pprint(f.num_empty_seats())
+
+f.print_cards(card_printer)
+
+# pprint(f.seating_plan)
